40fd3ecf80ad78713d5b1da00efad6519debc5d0
linvanda <1049180458@qq.com>
2017-10-18 15:35:22 +0800

    <feature>test

--- test2.py
+++ test2.py
@@ -1,9 +1,12 @@
 # coding:utf-8
 
-import command
-import iglobal
 import os
 
-iglobal.BASE_DIR = os.getcwd().replace('\\', '/')
-
-command.Command.exec_hook('product', 'post', 'vmember')
+print '=========================================' + os.getcwd()
+# import command
+# import iglobal
+# import os
+#
+# iglobal.BASE_DIR = os.getcwd().replace('\\', '/')
+#
+# command.Command.exec_hook('product', 'post', 'vmember')
